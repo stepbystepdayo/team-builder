627793ba6d10108a5ab6f803e868f9f92312275e
Sayo <sl.deve080412@gmail.com>
2021-07-10 11:48:03 -0700

    I finished MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import defaultFriends from "./data";
+import { useState } from "react";
+
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  username: "",
+  email: "",
+  ///// DROPDOWN /////
+  role: "",
+};
 
 function App() {
+  const [friends, setFriends] = useState(defaultFriends);
+
+  const [values, setValues] = useState(initialFormValues);
+  // console.log("this is", formValues);
+
+  const onChange = (event) => {
+    // console.log("yes we changed!");
+    const name = event.target.name;
+    const value = event.target.value;
+    update(name, value);
+  };
+
+  const update = (inputName, inputValue) => {
+    const newValues = {
+      ...values,
+      [inputName]: inputValue,
+    };
+    setValues(newValues);
+  };
+
+  const submit = (evt) => {
+    const { username, email, role } = values;
+    evt.preventDefault();
+    if ()alert
+      setFriends([
+        ...friends,
+
+        { username: username.trim(), email: email.trim(), role: role.trim() },
+      ]);
+
+    setValues(initialFormValues);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="title">My Team Member List</h1>
+        {friends.map((friend) => {
+          return (
+            <h3 key={friend.id}>
+              I am {friend.username} and {friend.role}
+              <br />
+              My email is {friend.email}
+            </h3>
+          );
+        })}
+        {/* make the form */}
+        <form onSubmit={submit}>
+          <label htmlFor="usernameInput">
+            username:
+            <input
+              onChange={onChange}
+              type="text"
+              name="username"
+              value={values.username}
+            />
+          </label>
+          <br />
+          <label htmlFor="emailInput">
+            Email:
+            <input
+              onChange={onChange}
+              type="text"
+              name="email"
+              value={values.email}
+            />
+          </label>
+          <br />
+          <label>
+            Role:
+            <select onChange={onChange} name="role" value={values.role}>
+              <option> --- Select a Role --- </option>
+              <option>Designer</option>
+              <option>Developer</option>
+              <option>Security Dog</option>
+            </select>
+          </label>
+          <br />
+
+          <button>Submit!</button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ team-builder/src/data.js
@@ -0,0 +1,28 @@
+const initialFriendsList = [
+  {
+    id: 1,
+    username: "Sayo",
+    email: "lovingfoss-s@gmail.com",
+    role: "Designer",
+  },
+  {
+    id: 2,
+    username: "Kai",
+    email: "lovingfoss-k@gmail.com",
+    role: "Developer",
+  },
+  {
+    id: 3,
+    username: "Milla",
+    email: "lovingfoss-m@gmail.com",
+    role: "Dog",
+  },
+  {
+    id: 4,
+    username: "Totti",
+    email: "lovingfoss-t@gmail.com",
+    role: "Dog",
+  },
+];
+
+export default initialFriendsList;
